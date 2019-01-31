4f7c8be0c476cb01ca55c8051fb89a928e693a51
ahaavisto <aino.r.haavisto@helsinki.fi>
2019-01-31 11:14:50 +0200

    lisää plotteja + järjestysten vertailua

--- gradukoodi/plot.py
+++ gradukoodi/plot.py
@@ -13,7 +13,9 @@ with open ('heisig_order.txt', 'r') as f:
 		i += 1
 		if i >= minne_asti:
 			break
-'''
+			
+#jos mukana komponentit
+
 algo = []
 with open ('tuloksia/pelkka_algon_tulos_BCCWJ.txt', 'r') as f:
 	i = 0
@@ -23,6 +25,8 @@ with open ('tuloksia/pelkka_algon_tulos_BCCWJ.txt', 'r') as f:
 		if i >= minne_asti:
 			break
 '''
+
+#jos mukana vain jooyoo-kanjit
 algo = []
 with open ('tuloksia/algon_jooyoo_kanjit.txt', 'r') as f:
 	i = 0
@@ -31,7 +35,7 @@ with open ('tuloksia/algon_jooyoo_kanjit.txt', 'r') as f:
 		i += 1
 		if i >= minne_asti:
 			break
-			
+'''		
 bkb = []
 with open ('bkb1.txt', 'r') as f:
 	i = 0
@@ -47,41 +51,42 @@ with open ('freq_BCCWJ.txt', 'r') as f:
 	for rivi in f:
 		frek.append(rivi.split('\t')[0])
 		i += 1
-		if i >= minne_asti:
+		if i >= 500:
 			break
-	
-algon_tulos = []	
-i = 0
-for juttu in algo:
-	for kanji in frek:
-		if kanji == juttu:
-			i += 1
-	algon_tulos.append(i)
-	
-heisig_tulos = []	
-i = 0
-for juttu in heisig:
-	for kanji in frek:
-		if kanji == juttu:
-			i += 1
-	heisig_tulos.append(i)	
-	
-bkb_tulos = []	
-i = 0
-for juttu in bkb:
-	for kanji in frek:
-		if kanji == juttu:
-			i += 1
-	bkb_tulos.append(i)	
+
+def luo_plot(inputti, output):
+	i = 0
+	ei_leikkaus = ""
+	leikkaus = ""
+	onko_leikkauksessa = False
+	for merkki in inputti:
+		for kanji in frek:
+			if kanji == merkki:
+				i += 1
+				leikkaus += kanji + ' '
+				onko_leikkauksessa = True
+				break				
+		if not onko_leikkauksessa:
+			ei_leikkaus += merkki + ' '
+		onko_leikkauksessa = False			
+		output.append(i)
+	print("Opiskelujarjestyksen ja yleisimpien leikkaus:")
+	print(leikkaus)
+	print("Opiskelujarjestyksessa olleet, ei-yleiset merkit:")
+	print(ei_leikkaus)
+	return output
+
+bkb_tulos = luo_plot(bkb, [])
+algon_tulos = luo_plot(algo, [])
+heisig_tulos = luo_plot(heisig, [])
 
 plt.plot(range(1, minne_asti+1), color='violet', label='Yleisyysjarjestys')
 plt.plot(algon_tulos, label='Algoritmin tulos')
-plt.plot(heisig_tulos, color='green', label='Heisig')
 plt.plot(bkb_tulos, color='red', label='BKB 1')
+plt.plot(heisig_tulos, color='green', label='Heisig')
 
 plt.ylabel('Osuus korpuksen yleisimmista merkeista')
 plt.xlabel('Opiskellut merkit')
 pylab.legend(loc='upper left')
 plt.axis([1, minne_asti, 1, minne_asti])
-plt.show()
-
+#plt.show()
