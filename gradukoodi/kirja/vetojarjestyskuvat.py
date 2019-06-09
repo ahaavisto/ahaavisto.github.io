cd25220710138cecf3e43e9383d2e849d245fa1c
ahaavisto <aino.r.haavisto@helsinki.fi>
2019-06-09 11:06:37 +0300

    yritetty säätää vetojärjestyskuvia

--- gradukoodi/kirja/vetojarjestyskuvat.py
+++ gradukoodi/kirja/vetojarjestyskuvat.py
@@ -1,10 +1,32 @@
 import shutil
+import os
+
+import xml.etree.ElementTree as ET
+
+puu = ET.parse('../kanjidic2.xml')
+juuri = puu.getroot()
 
 kanit = []
 
-with open ('kirja/tuloste.tsv') as f:
+with open ('tuloste.tsv') as f:
 	for entry in f:
 		kanit.append(entry[0])
 
-for file in dir:
-	
+vedot = os.listdir('../../../kanji_stroke_order')
+
+sanakirja = {}
+
+for kanji in juuri.findall('character'):
+	merkki = kanji.find('literal').text
+	heksa = hex(ord(merkki))
+	heksa = heksa.replace('x', '')
+	sanakirja[heksa + '.svg'] = merkki
+
+
+for kuva in vedot:
+	if kuva in sanakirja:
+		path = '../uuet/'+sanakirja[kuva]
+		shutil.copy('../../kanji_stroke_order/'+kuva, path)
+		print('moi')
+	else:
+		print('ei')
