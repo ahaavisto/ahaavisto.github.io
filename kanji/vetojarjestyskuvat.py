c42e99ecbd5b0ce743e8e4465595b7a5087d2908
ahaavisto <ainorosah+github@gmail.com>
2021-08-14 20:15:37 +0300

    Lisätty 4. osan kanjit

--- kanji/vetojarjestyskuvat.py
+++ kanji/vetojarjestyskuvat.py
@@ -9,9 +9,11 @@ juuri = puu.getroot()
 kanit = []
 
 #joku lista jossa jooyoo + lisää merkkejä
-with open ('../gradukoodi/tuloksia/centrality_of_kanjis_BCCWJ.txt') as f:
+with open ('4osa.txt') as f:
 	for entry in f:
-		kanit.append(entry[0])
+		kanji = entry.split('\t')[1][-2]
+		kanit.append(kanji)
+		print(kanji)
 
 vedot = os.listdir('../../kanji_stroke_order')
 
@@ -28,7 +30,7 @@ for kanji in juuri.findall('character'):
 for kuva in vedot:
 	if kuva in sanakirja and sanakirja[kuva] in kanit:
 		print(kuva)
-		path = '../../vedot_kaikki/' + sanakirja[kuva] + '.svg'
+		path = '../../ippoippovedot/' + sanakirja[kuva] + '.svg'
 		try:
 			shutil.copy('../../kanji_stroke_order/'+kuva, path)
 		except FileNotFoundError as e: 
